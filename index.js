f592f22fd370539a1eeeac3dbed2db4a727984e1
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-15 12:04:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file
+// Code you
+
+function findMatching(drivers, string){
+    return drivers.filter( function (drivername){
+      return drivername.toLowerCase() === string.toLowerCase()
+    })
+}
+
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
