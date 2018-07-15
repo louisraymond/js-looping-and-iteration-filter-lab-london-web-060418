abb9d42f5b4cfc27dea1d79cfc9522757ed11419
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-15 11:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code you
 
 function findMatching(drivers, string){
-    return drivers.filter( function (drivername){
+    return drivers.filter( function (string){
       return drivername.toLowercase() === name.toLowercase
     })
 }
+
+funtion fuzzyMatch(drivers,)
